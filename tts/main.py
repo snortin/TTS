04c6205f486b6e06934ab2f7316394cd5fca71b0
snot <105960344+snortin@users.noreply.github.com>
2022-06-24 01:34:16 -0400

    Add files via upload

---
+++ tts/main.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+import os 
+
+text = input('text: ')
+
+save = input('name of file?')
+
+lang = 'en'
+
+obj = gTTS(text = text, lang = lang, slow=False)
+
+saved_file = obj.save(f'{save}.mp3')
+os.system(f'mpg321{saved_file}')
