e9a88b71df8afdaacc2c4de05f32fdea1f9a01de
snot <105960344+snortin@users.noreply.github.com>
2022-06-24 01:35:03 -0400

    Update main.py

--- tts/main.py
+++ tts/main.py
@@ -2,11 +2,8 @@ from gtts import gTTS
 import os 
 
 text = input('text: ')
-
 save = input('name of file?')
-
 lang = 'en'
-
 obj = gTTS(text = text, lang = lang, slow=False)
 
 saved_file = obj.save(f'{save}.mp3')
